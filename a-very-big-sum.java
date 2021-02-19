8131edb502c42aff2803685357eda500015e56ba
Javi Alarcia <jabogood@gmail.com>
2021-02-19 23:08:44 +0100

    Create a-very-big-sum.java

---
+++ a-very-big-sum.java
@@ -0,0 +1,10 @@
+    // Return the sum of big integers in an array
+    static long aVeryBigSum(long[] ar) {
+        long sum = 0;
+        
+        for (int i=0; i<ar.length; i++){
+            sum += ar[i];
+        }
+        
+        return sum;
+    }
